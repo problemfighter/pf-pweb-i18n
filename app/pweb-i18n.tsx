026a6dcadb6f352862deb72f0994bf401cdd4d7a
hmtmcse <hmtm.cse@gmail.com>
2022-05-12 23:21:59 +0600

    added number translation

--- app/pweb-i18n.tsx
+++ app/pweb-i18n.tsx
@@ -23,6 +23,25 @@ export const _t = (text: any) => {
     return i18next.t(text)
 }
 
+const numberTranslation: any = {
+    "bn": {0: "০", 1: "১", 2: "২", 3: "৩", 4: "৪", 5: "৫", 6: "৬", 7: "৭", 8: "৮", 9: "৯"}
+}
+
+export const _tNumber = (text: any) => {
+    if (!text) {
+        return text
+    }
+    text += ""
+    let lang: string = i18n.language
+    let translate = numberTranslation[lang]
+    if (translate) {
+        for (let key in translate) {
+            text = text.replaceAll(key, translate[key]);
+        }
+    }
+    return text
+}
+
 export const _loadTranslation = (lang: string, translation: any) => {
     if (!resources[lang] || !resources[lang].translation) {
         resources[lang] = {translation: {}}
